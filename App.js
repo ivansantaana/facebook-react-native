769e38f14aba81983135389d63df485c65b4d514
Ivan <ivansantanacalle@gmail.com>
2022-01-06 18:43:42 +0100

    Header component do it and added to the app.js

--- App.js
+++ App.js
@@ -1,20 +1,21 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import 'react-native-gesture-handler';
+import React from 'react';
+import { StatusBar, ScrollView } from 'react-native';
 
-export default function App() {
+import Header from './components/Header'
+
+const App = () => {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <>
+      <StatusBar
+        backgroundColor= "#FFFFFF"
+        barStyle="dark-content"
+      />
+      <ScrollView>
+        <Header />
+      </ScrollView>
+    </>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App
