caefb0e6881cca36f501aaab8a5c579f5713764d
Ivan <ivansantanacalle@gmail.com>
2022-01-07 00:25:22 +0100

    Tried to solve Post.js error

--- components/Post.js
+++ components/Post.js
@@ -8,8 +8,7 @@ import MainSeparator from './MainSeparator'
 
 const Container = styled.View`
     flex: 1;
-`;
-
+`
 const Header = styled.View`
     flex-direction: row;
     align-items: center;
@@ -17,47 +16,39 @@ const Header = styled.View`
     height: 50px;
     margin-top: 5px;
     padding: 0 10px;
-`;
-
+`
 const Row = styled.View`
     flex-direction: row;
     align-items: center;
-`;
-
+`
 const User = styled.Text`
     font-size: 12px;
     font-weight: bold;
     color: #222121;
-`;
-
+`
 const Time = styled.Text`
     font-size: 9px;
     color: #747476;
-`;
-
+`
 const PostContent = styled.Text`
     font-size: 12px;
     color: #222121;
     line-height: 16px;
     padding: 0 11px;
-`;
-
+`
 const Photo = styled.Image`
     margin-top: 9px;
     width: 100%;
     height: 300px;
-`;
-
+`
 const Footer = styled.View`
     padding: 0 11px;
-`;
-
+`
 const FooterCount = styled.View`
     flex-direction: row;
     justify-content: space-between;
     padding: 9px 0;
-`;
-
+`
 const IconCount = styled.View`
     background: #1878f3;
     width: 20px;
@@ -66,42 +57,36 @@ const IconCount = styled.View`
     align-items: center;
     justify-content: center;
     margin-right: 6px;
-`;
-
+`
 const TextCount = styled.Text`
     font-size: 11px;
     color: #424040;
-`;
-
+`
 const Separator = styled.View`
     width: 100%;
     height: 1px;
     background: #f9f9f9;
-`;
-
+`
 const FooterMenu = styled.View`
     flex-direction: row;
     justify-content: space-between;
     padding: 9px 0;
-`;
-
+`
 const Buttom = styled.TouchableOpacity`
     flex-direction: row;
-`;
-
+`
 const Icon = styled.View`
     margin-right: 6px;
-`;
-
+`
 const Text = styled.Text`
     font-size: 12px;
     color: #424040;
-`;
+`
 
 const Post = () =>{
     return(
         <>
-        {fakeUsers.map((user, i) =>{
+        {fakeUsers.map((user, i) => {
             return(
                 <Container key={i.toString()}>
                     <Header>
