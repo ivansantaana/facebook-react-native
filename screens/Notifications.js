b64305f0182c802a52d1bf03fd08e100d5a97184
Ivan <ivansantanacalle@gmail.com>
2022-01-07 09:08:49 +0100

    Notifications bar

--- screens/Notifications.js
+++ screens/Notifications.js
@@ -1,16 +1,39 @@
 import React from 'react';
-import { Text } from 'react-native';
 import styled from 'styled-components/native';
+import { MaterialCommunityIcons } from '@expo/vector-icons'
 
-const View = styled.View`
+const Container= styled.View`
+    width: 100%;
+    height: 60px;
+    flex-direction: row;
     align-items: center;
-    height: 500px;
+    background: #FFFFFF;
 `
+const Text = styled.Text`
+    color: #000000;
+    font-size: 18px;
+    font-weight: bold;
+    padding-left: 6px;
+`
+const Button = styled.TouchableOpacity`
+    width: 42px;
+    height: 42px;
+    border-radius: 21px;
+    margin-left: 53%;
+    background: #EEEEEE;
+    align-items: center;
+    justify-content: center;
+`
+
 const Notifications = () => {
     return (
-        <View>
-            <Text>Notifications Screen</Text>
-        </View>
+        <Container>
+            <Text>Notificaciones</Text>
+            <Button>
+                <MaterialCommunityIcons name='magnify' size={26} color='black' />
+            </Button>
+
+        </Container>
     );
 }
 
